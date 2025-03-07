c0e78f9eb9310d03f90704702737c7d3c3a30612
dawnstoryrevelation <musicaljasperj@gmail.com>
2025-03-07 21:26:58 +1100

    s

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: './tailwind.config.ts' // Specify the path to your Tailwind config file
+    },
     autoprefixer: {},
   },
 };
