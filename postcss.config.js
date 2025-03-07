ae3b430f9dece11652f40e48104f5fdd14881e45
dawnstoryrevelation <musicaljasperj@gmail.com>
2025-03-07 21:05:11 +1100

    vo

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss7-compat': {}, // Add this line
-    autoprefixer: {}, // This is already there
-  },
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 };
